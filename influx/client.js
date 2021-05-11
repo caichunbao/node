c6b6e52ab1a73d5e3136a421155f8c48c243ef4c
蔡春保 <chunbao.cai@17zuoye.com>
2021-05-11 20:08:58 +0800

    add influx client.js

--- influx/client.js
+++ influx/client.js
@@ -1,7 +1,7 @@
 const {InfluxDB} = require('@influxdata/influxdb-client')
 
 // You can generate a Token from the "Tokens Tab" in the UI
-const token = 'S6e9mkuTTAQELT8O-aAaxh0D5XPAW5zPJonTh4CUrYjvvOcBjky3SZDFwrJbWcUndGraFRzXxin-Bj-s-2t64A=='
+const token = '4wtAonkhlh85K1O7-jW-DPlLN9KKy2zGndhVUZ02PJiNOTnNauICAMlPEmDC93fvGJlM9rD1LBmIzWpwLPD5bg=='
 const org = '17zy'
 const bucket = 'bucket_name_demo'
 
@@ -9,10 +9,11 @@ const client = new InfluxDB({url: 'http://localhost:8086', token: token})
 
 const {Point} = require('@influxdata/influxdb-client')
 const writeApi = client.getWriteApi(org, bucket)
-writeApi.useDefaultTags({host: 'host1'})
+writeApi.useDefaultTags({host: 'host2'})
 
 const point = new Point('mem')
   .floatField('used_percent', 23.43234543)
+  .floatField('used_percent1', 23.43234543)
 writeApi.writePoint(point)
 writeApi
     .close()
