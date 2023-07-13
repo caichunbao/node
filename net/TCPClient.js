1c9458bd2f62a33757b9ae95ac52b8dbde2ad624
caichunbao <vipcaichunbao@163.com>
2023-07-13 15:26:11 +0800

    .

--- net/TCPClient.js
+++ net/TCPClient.js
@@ -6,10 +6,14 @@
 const net = require("net")
 const HOST = "127.0.0.1"
 const PORT = "10010"
+const LOCALPORT = 2001
 
 let client = new net.Socket();
-
-client.connect(PORT, HOST, function(){
+client.connect({
+    host: HOST,
+    port: PORT,
+    localPort: LOCALPORT
+}, function(){
 
     console.log(`CONNECTED TO: ${HOST}:${PORT}`);
 
